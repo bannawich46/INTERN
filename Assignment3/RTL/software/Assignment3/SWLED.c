f2592f9a80b66eb3ffb3dc337eb8fc53e360f17d
bannawich46 <bannawich46@gmail.com>
2024-06-14 15:16:03 +0700

    Edit Software for Blink LED and on/off LED using SW

--- Assignment3/RTL/software/Assignment3/SWLED.c
+++ Assignment3/RTL/software/Assignment3/SWLED.c
@@ -7,39 +7,26 @@
 /*
  * presspb_led_nios2.c
   */
-#include "sys/alt_stdio.h"
+#include <stdio.h>
 #include "system.h"
+#include "altera_avalon_pio_regs.h"
+#include "unistd.h" // For usleep function
 
-#define LED_BLINK_DELAY 250000 // Blink delay for LED2 (every 1 seconds)
 int main()
 {
-	int switch_value;
-	int led_value = 0;
-	int counter = 0;
-
-	    while (1) {
-	        // Read the switch value
-	        switch_value = *(volatile unsigned char *) SW_PIO_BASE;
-
-	        // Control LED1 with the switch
-	        if (switch_value & 0x01) {
-	            led_value |= 0x01; // Set the last bit of led_value to 1
-	        } else {
-	            led_value &= ~0x01; // Set the last bit of led_value to 0
-	        }
-
-	        // Blink LED2 every 0.5 seconds
-	        counter++;
-	        if (counter >= LED_BLINK_DELAY) {
-	            counter = 0;
-	            led_value ^= 0x02; // Toggle the second bit of led_value
-	        }
-
-	        // Write the value to the LED
-	        *(volatile unsigned char *) LED_PIO_BASE = led_value;
-	    }
-   return (0);
+    while (1)
+    {
+    	int switch_state = IORD_ALTERA_AVALON_PIO_DATA(SW_PIO_BASE);
+    	if (switch_state) {
+    		// Toggle the LED state
+    		IOWR_ALTERA_AVALON_PIO_DATA(LED_PIO_BASE, 0x01);
+    		usleep(500000);
+    		IOWR_ALTERA_AVALON_PIO_DATA(LED_PIO_BASE, 0x00);
+    		usleep(500000);
+    	}
+    	else {
+    		IOWR_ALTERA_AVALON_PIO_DATA(LED_PIO_BASE, 0x00);
+    	}
+    }
+    return 0;
 }
-
-
-
